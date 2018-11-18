e312852cbdfb0034df100d0af06ccef300464a80
kulia26 <kulinich261@gmail.com>
2018-11-18 12:53:44 +0200

    Create Game widget

---
+++ game.cpp
@@ -0,0 +1,17 @@
+﻿#include "game.h"
+#include "ui_game.h"
+
+Game::Game(QWidget *parent) :
+  QWidget(parent),
+  ui(new Ui::Game)
+{
+  ui->setupUi(this);
+  this->setFixedSize(800,600);
+  this->setPalette(QPalette(QColor(255,255,255,0)));
+  this->setWindowTitle("Galaga");
+}
+
+Game::~Game()
+{
+  delete ui;
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "game.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+  QApplication a(argc, argv);
+  Game w;
+  w.show();
+
+  return a.exec();
+}
